b519d4fec456b7eea8221e950082a394311d5f73
Kevin Saephanh <Kevin.Saephanh@gmail.com>
2023-12-31 13:37:40 -0600

    Add vite config for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path, { resolve } from 'path';
 
-// https://vitejs.dev/config/
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const rootDir = resolve(__dirname);
+const srcDir = resolve(rootDir, 'src');
+const scriptsDir = resolve(srcDir, 'scripts');
+const outDir = resolve(rootDir, 'dist');
+
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   plugins: [react()],
-})
+  build: {
+    outDir,
+    minify: isProd,
+    modulePreload: false,
+    reportCompressedSize: isProd,
+    emptyOutDir: !isDev,
+    rollupOptions: {
+      input: {
+        content: resolve(scriptsDir, 'content', 'index.ts'),
+        background: resolve(scriptsDir, 'background', 'index.ts'),
+        popup: resolve(scriptsDir, 'popup', 'index.html'),
+        options: resolve(scriptsDir, 'options', 'index.html'),
+      },
+      output: {
+        entryFileNames: 'src/scripts/[name]/index.js',
+        assetFileNames: 'assets/[ext]/[name].[ext]',
+      },
+    },
+  },
+});
